e7475df106ef33fe335d5ae2a65ed1cda8406239
Shuta-syd <shuta.ogura0921@gmail.com>
2022-12-10 16:29:22 +0900

    [add] setupSocket() added

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -4,9 +4,12 @@
 #include <iostream>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/poll.h>
 #include <unistd.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <fcntl.h>
+#include <vector>
 
 #define BACKLOG 10
 
@@ -20,9 +23,12 @@ public:
 
 	// start point
 	void start();
-	void startPoll();
+	void setupSocket();
 
 private:
-	int port_;
+	int port_; // port number to connect to client
+	int nfds_; // number of connected file  descriptor
+	int master_sd_; // server socket file descriptor
 	std::string password_;
+	std::vector<struct pollfd> pollfds_;
 };
--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -6,82 +6,62 @@ Server::Server(int port, std::string password): port_(port), password_(password)
 
 Server::~Server() {}
 
-
 /**
- *
- */
+ *@brief 
+*/
 void Server::start() {
-	int sockfd = socket(PF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0)
-	{
-		std::cerr << "socket error: " << __LINE__ << std::endl;
-		exit(1);
-	}
-
-	// Set server IP address and port information
-	struct sockaddr_in serverAddr, clientAddr;
-	bzero(&serverAddr, sizeof(serverAddr)); //init by 0
-	serverAddr.sin_family = PF_INET; //address family
-	serverAddr.sin_port = htons(this->port_); //sin_port's size is uint_16
-	serverAddr.sin_addr.s_addr = INADDR_ANY;	// IP 0.0.0.0
-
-	if (bind(sockfd, reinterpret_cast<struct sockaddr *>(&serverAddr), sizeof(serverAddr)) < 0)
-	{
-		std::cerr << "bind error: " << __LINE__ << std::endl;
-		exit(1);
-	}
-
-	if (listen(sockfd, BACKLOG) < 0)
-	{
-		std::cerr << "bind error: " << __LINE__ << std::endl;
-		close(sockfd);
-		exit(1);
-	}
-
-	std::cout << "Waiting for connect... [PORT]: " << port_ << " [PASSWORD]: " << password_ << std::endl;
-	while (1)
-	{
-		std::cout << 1 << std::endl;
-		socklen_t addrlen = sizeof(clientAddr);
-		int clientSockfd = accept(sockfd, reinterpret_cast<struct sockaddr *>(&clientAddr), &addrlen);
-		std::cout << clientSockfd << std::endl;
-		if (clientSockfd < 0)
-		{
-			std::cerr << "accept error: " << __LINE__ << std::endl;
-			close(sockfd);
-			exit(1);
-		}
-		std::cout << "Connected: " << clientAddr.sin_addr.s_addr << std::endl;
-
-	while (1)
-	{
-		char buf[1024];
-		int revSize = recv(clientSockfd, buf, 1024, 0);
-		std::cout << buf << std::endl;
-		if (revSize < 0)
-		{
-			std::cerr << "recv error: " << __LINE__ << std::endl;
-			close(sockfd);
-			close(clientSockfd);
-			exit(1);
-		}
-		int sendSize = send(clientSockfd, buf, 1024, 0);
-		if (sendSize < -1)
-		{
-			std::cerr << "send error: " << __LINE__ << std::endl;
-			close(sockfd);
-			close(clientSockfd);
-			exit(1);
-		}
-	}
-	}
+	this->setupSocket();
 }
 
-void Server::startPoll() {
-	int sockfd = socket(PF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0)
+/**
+ * @brief to set up a server socket
+ */
+void Server::setupSocket() {
+	int enable = 1;
+
+	/* server socket create */
+	this->master_sd_ = socket(AF_INET, SOCK_STREAM, 0);
+	if (this->master_sd_ < 0)
 	{
-		std::cerr << "socket error: " << __LINE__ << std::endl;
-		exit(1);
+		std::cerr << "socket error" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	/* set socket option to server socket */
+	if (setsockopt(this->master_sd_, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable)) < 0)
+	{
+		std::cerr << "setsockopt error" << std::endl;
+		close(this->master_sd_);
+		exit(EXIT_FAILURE);
+	}
+
+	/* Change server socket fd from blocking IO to non-blocking IO*/
+	if (fcntl(this->master_sd_, O_NONBLOCK, &enable))
+	{
+		std::cerr << "fcntl O_NONBLOCK error" << std::endl;
+		close(this->master_sd_);
+		exit(EXIT_FAILURE);
+	}
+
+	/* create address for server socket fd */
+	struct sockaddr_in addr;
+	bzero(&addr, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_port = htons(port_);
+
+	/* bind address to server socket fd*/
+	if (bind(this->master_sd_, (struct sockaddr*)&addr, sizeof(addr)) < 0)
+	{
+		std::cerr << "bind error" << std::endl;
+		close(this->master_sd_);
+		exit(EXIT_FAILURE);
+	}
+
+	/* try to specify maximum of SOCKET_MAX pending connections for the server socket */
+	if (listen(this->master_sd_, BACKLOG) < 0)
+	{
+		std::cerr << "listen error" << std::endl;
+		exit(EXIT_FAILURE);
 	}
 }
