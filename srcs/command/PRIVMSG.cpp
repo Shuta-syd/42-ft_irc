43070ab7b57f7772cd6142ef5bbd58f33929a179
kny-i <kny_i@keio.jp>
2022-12-18 18:00:44 +0900

    add is correct fmt

--- srcs/command/PRIVMSG.cpp
+++ srcs/command/PRIVMSG.cpp
@@ -1,4 +1,22 @@
-#include <Server.hpp>
+#include "../../inc/Server.hpp"
 
+bool is_correct_fmt(std::vector<std::string> const &params)
+{
+	return true;
+}
 
+/**
+ * @brief execute privmsg command
+ *
+ * PRIVMSG <target> <message to send>
+ */
 
+void PRIVMSG(Client &client)
+{
+	std::vector<std::string> const &params = client.getParams();
+	if (is_correct_fmt(params) == false) {
+		return;
+	}
+	/* check if client sends msg to channnel or not  */
+
+}
