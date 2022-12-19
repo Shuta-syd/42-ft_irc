dd3535e40aed9e6828bf6a5e1bf3000a6ffdab2e
Shuta-syd <shuta.ogura0921@gmail.com>
2022-12-19 15:30:19 +0900

    [fix] compile error fixed

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -7,15 +7,21 @@ class Channel
 {
 public:
 	Channel();
+	Channel(const Channel&);
 	~Channel();
+	Channel &operator=(const Channel &other);
+
 	const std::vector<Client> &getMember() const { return members_; }
 	const std::string &getTopic() const { return topic_; }
 	const std::string &getName() const { return name_; }
-	const std::string &getKey_() const { return key_; }
+	const std::string &getKey() const { return key_; }
+	Client *getOper() const { return oper_; }
 	void setMember(Client &member) { members_.push_back(member); }
 	void setName(std::string name) { name_ = name; }
+	void setKey(std::string key) { key_ = key; }
 	void setTopic(std::string topic) { topic_ = topic; }
-	void setOperator(Client *oper) { oper_ = oper; }
+	void setOper(Client *oper) { oper_ = oper; }
+
 
 private:
 	std::string name_;
--- srcs/command/JOIN.cpp
+++ srcs/command/JOIN.cpp
@@ -53,7 +53,7 @@ void enterChannel(
 	const int &fd = client.getFd();
 	const std::string &nick = client.getNickname();
 	Channel &channel= allChannel[channelName];
-	const std::string &channelKey = channel.getKey_();
+	const std::string &channelKey = channel.getKey();
 	const std::vector<Client> &members = channel.getMember();
 
 	if (channelName == "0") {
@@ -63,7 +63,7 @@ void enterChannel(
 	else if (channelName != channel.getName() && (key == channelKey || channelKey == "")) {
 		channel.setName(channelName);
 		channel.setMember(client);
-		client.setChannel(channelName, channel);
+		// client.setChannel(channelName, channel);
 		sendMessage(fd, JOIN_MESSAGE(nick, channelName), 0);
 		//sendMessage(fd, MODE_MESSAGE(channelName, "+nt"), 0);
 		// sendMessage(fd, RPL_NAMREPLY(nick, channelName), 0);
@@ -71,7 +71,8 @@ void enterChannel(
 	}
 	else if (key == channelKey || channelKey == "") { // already exist
 		channel.setMember(client);
-		client.setChannel(channelName, channel);
+		// client.setChannel(channelName, channel);
+		// client.setChannel(channelName, channel);
 		std::cout << MGN << "channel already exist. JOIN [" << channelName << "]" << RES << std::endl;
 		for (size_t i = 0; i < members.size(); i++) {
 			const int &fd = members.at(i).getFd();
