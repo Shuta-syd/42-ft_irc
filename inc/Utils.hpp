aa0f8376548559446c692233261bdd056ac840bf
vim <cant_reply.com>
2022-12-19 19:19:20 +0900

    戻り値:vector<string>\n引数(string, string)のsplit追加(in Utils.cpp)

--- inc/Utils.hpp
+++ inc/Utils.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 int find(const std::string &str, const std::string &target);
+std::vector<std::string> split(std::string str, std::string del);
