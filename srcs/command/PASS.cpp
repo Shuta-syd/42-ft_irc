dd904450b36935b8e5889177d4a77e5aead8bf75
vim <cant_reply.com>
2022-12-19 16:35:31 +0900

    if you don't have only 1 param, you reply error message #10

--- srcs/command/PASS.cpp
+++ srcs/command/PASS.cpp
@@ -6,13 +6,20 @@
  * parameters <password>
  * <in short> [PASS CMD] check whether server should pass the client(user).
  */
+
+
+#define ERR_NEEDMOREPARAMS(nick) ":ft_irc 461 " + nick + " pass :Not enough parameters\n"
+#define ERR_ALREADYREGISTRED(nick)	":ft_irc 462 " + nick + " :You may not reregister\n"
+#define RPL_NONE(message) ":ft_irc 300 * :" + message + "\n"
+#define ERR_PASSWDMISMATCH(nick) ":ft_irc 464 " + nick + " :Password incorrect\n"
+
 void PASS(Client &client, std::string const &server_password)
 {
 	int const &fd = client.getFd();
 	std::string const &nick = client.getNickname();
 	std::string const &password = client.getParams()[0];
 	if (client.getParams().size() != 1) {
-//		sendMessage(fd, );
+		sendMessage(fd, ERR_NEEDMOREPARAMS(nick), 0);
 		return ;
 	}
 	if (password != server_password)
