507a6a00cab83f1d23f609ddbaf678d6f57d173c
vim <cant_reply.com>
2022-12-20 19:46:28 +0900

    frightend person <-> client check whether both is exist on channel (#22)

--- srcs/command/KICK.cpp
+++ srcs/command/KICK.cpp
@@ -5,7 +5,7 @@
  * @params
  * <channel> <user> [<comment>]
  * [important point] only channel operator can kick them out
- *
+ * #issue 22
  */
 
 bool is_nick_in_channel(std::string const &nick, Channel &channel) {
@@ -21,16 +21,17 @@ bool is_nick_in_channel(std::string const &nick, Channel &channel) {
 
 void KICK(Client &client, std::map<std::string, Channel> channels) {
 	int fd = client.getFd();
-	std::string nick = client.getNickname();
+	std::string const &nick = client.getNickname();
 	Channel channel = channels[client.getParams()[0]];
+	std::string const &frightened_person = client.getParams()[1];
 
 	if (client.getParams()[0].size() < 2) {
 		sendMessage(fd, ERR_NEEDMOREPARAMS(nick, "KICK"), 0);
 	} else if (channel.getOper() != nick) {
 		sendMessage(fd, ERR_NOPRIVILEGES(nick), 0);
+	} else if (is_nick_in_channel(frightened_person, channel) == false) {
+		sendMessage(fd, ERR_NOSUCHNICK(frightened_person), 0);
 	} else if (is_nick_in_channel(nick, channel) == false) {
 		sendMessage(fd, ERR_NOTONCHANNEL(nick, channel.getName()), 0);
-	} else {
-		;
 	}
 }
