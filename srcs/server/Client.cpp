e83a2ffc763a25e449e98ed8249b9fdf2a38406e
kny-i <kny_i@keio.jp>
2022-12-20 21:19:20 +0900

    make ok

--- srcs/server/Client.cpp
+++ srcs/server/Client.cpp
@@ -106,7 +106,7 @@ void Client::clearParsedMessage() {
 	params_.clear();
 }
 
-const std::map<std::string, Channel> &Client::getChannels() { return channels_; }
+std::map<std::string, Channel> &Client::getChannels() { return channels_; }
 
 void Client::setChannel(const std::string &name, Channel &channel) {
 	channels_[name] = channel;
