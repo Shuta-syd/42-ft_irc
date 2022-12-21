2690edb74c1639160801b54d5998563eff2e9ef5
kny-i <kny_i@keio.jp>
2022-12-21 18:14:28 +0900

    edit funcs

--- srcs/server/Channel.cpp
+++ srcs/server/Channel.cpp
@@ -19,8 +19,19 @@ void Channel::addOper(std::string name) { opers_.push_back(name); }
 
 bool Channel::is_inChannel(std::string const &nick_nme)  {
 
-	std::vector<Client> const &members = getMember();
+	std::vector<Client> const &members = this->getMember();
 	for (std::vector<Client>::const_iterator  it = members.begin(); it !=members.end(); it++) {
+		if (it->getNickname() == nick_nme) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool Channel::is_operator(const std::string &nick_name) {
+
+	std::vector<std::string> const  &members = this->getOper();
+	for (std::vector<Client>::const_iterator  it = members.begin(); it != members.end(); it++) {
 		if (it->getNickname() == nick_nme) {
 			return true;
 		}
