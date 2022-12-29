c03d7e8e3558370b92e1893dc481c746c51d9502
Shuta-syd <shuta.ogura0921@gmail.com>
2022-12-29 16:16:44 +0900

    [add] poll method added to bot #63

--- bot/Bot.cpp
+++ bot/Bot.cpp
@@ -11,23 +11,41 @@ Bot::~Bot(){}
  */
 void Bot::start() {
 	this->setupBotSocket();
+	this->createPoll();
 	this->authorize();
 	std::cout << BLU << "------CONNECTED------" << RES << std::endl;
 
 	while (1)	{
-		receive(bot_sd_);
+		std::cout << BLU << "-------------Waiting on poll()-------------" << RES << std::endl;
+		if (poll(&pollfd_, 1, TIMEOUT) == -1)
+			throw std::exception();
+		if (pollfd_.revents == POLLIN) { // Get incoming data
+			receive();
+		}
+		else if (pollfd_.revents == POLLOUT)
+		{ // Write to server
+		}
 	}
 }
 
 /**
  * @brief
  */
-void Bot::receive(int fd) {
+void Bot::receive() {
 	char message[MSG_MAX] = {0};
 
-	ssize_t bytes = recv(fd, message, sizeof(message), 0);
+	ssize_t bytes = recv(bot_sd_, message, sizeof(message), 0);
 	if (bytes < 0)
 		throw std::exception();
+}
+
+/**
+ * @brief Pass server authentication
+ */
+void Bot::authorize() {
+	sendMessage(bot_sd_, PASS_MESSAGE(password_), 0);
+	sendMessage(bot_sd_, NICK_MESSAGE(nick_), 0);
+	sendMessage(bot_sd_, USER_MESSAGE(nick_, "*", "*", nick_), 0);
 }
 
 /**
@@ -51,12 +69,14 @@ void Bot::setupBotSocket() {
 	}
 }
 
-
 /**
- * @brief Pass server authentication
+ * @brief to create polling method corresponding to the num of fds (include server socket fd)
+ * @param sockfd An indication of which fd for which PollMethod to create
+ *
  */
-void Bot::authorize() {
-	sendMessage(bot_sd_, PASS_MESSAGE(password_), 0);
-	sendMessage(bot_sd_, NICK_MESSAGE(nick_), 0);
-	sendMessage(bot_sd_, USER_MESSAGE(nick_, "*", "*", nick_), 0);
+void Bot::createPoll()
+{
+	pollfd_.fd = bot_sd_;
+	pollfd_.events = POLLIN | POLLOUT;
+	pollfd_.revents = 0;
 }
--- bot/Bot.hpp
+++ bot/Bot.hpp
@@ -4,28 +4,34 @@
 #include <string>
 #include <iostream>
 #include <sys/socket.h>
+#include <sys/poll.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <BotReply.hpp>
 
 #define MSG_MAX 1024
+#define TIMEOUT 3 * 60 * 1000
 
-class Bot {
+class Bot
+{
 public:
 	Bot();
 	Bot(int port, std::string password);
 	~Bot();
 
 	void start();
-	void receive(int fd);
-	void setupBotSocket();
-	void authorize();
 
 private:
 	int bot_sd_;
 	int port_;
 	std::string password_;
 	std::string nick_;
+	struct pollfd pollfd_;
+
+	void receive();
+	void setupBotSocket();
+	void createPoll();
+	void authorize();
 };
 
 #endif
